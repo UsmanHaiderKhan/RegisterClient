eb99d645b33e17a6c8de1a225f43db4ad8921c2b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2020-02-25 09:15:22 +0500

    ✔✨♻ update added modal for confirmation before deleting the record

--- admin.php
+++ admin.php
@@ -32,6 +32,7 @@
                         <table class="table display nowrap cell-border table-hover table-striped" id="table_data">
                             <thead class="thead-dark">
                                 <tr>
+                                     <th>Id</th>
                                     <th>Name</th>
                                     <th>Email</th>
                                     <th>Phone</th>
@@ -50,6 +51,7 @@
                                     foreach ($statement as $value) {
                                         ?>
                                         <tr>
+                                           <td><?php echo $value['id']; ?></td>
                                             <td><?php echo $value['name']; ?></td>
                                             <td><?php echo $value['email']; ?></td>
                                             <td><?php echo $value['phone']; ?></td>
@@ -62,8 +64,10 @@
                                           </td> -->
                                             <td><?php echo $value['message']; ?></td>
                                             <td>
-                                                <a href="editOrder.php?edit=<?php echo $value["id"]; ?>"><span class="btn btn-warning btn-sm">Edit</span></a>
-                                                <a href="deleteClient.php?delete=<?php echo $value["id"] ?>"><span class="btn btn-danger btn-sm">Delete</span></a>
+                                                <a  href="edit=<?php echo $value['id'] ?>" data-toggle="modal" data-target="#myModal"><span class="btn btn-warning btn-sm">Edit</span></a>
+                                                
+                                                 <button type="button" class="btn btn-danger delete_btn" >delete</button>
+                                                <!-- <a href="deleteClient.php?delete=<?php echo $value["id"] ?>"><span class="btn btn-danger btn-sm">Delete</span></a> -->
                                             </td>
                                         </tr> 
                                     <?php
@@ -76,7 +80,127 @@
               </div>
         </div>
     </div>
+<!--Edit Modal Area -->
 
+            <div class="modal" id="myModal">
+                  <div class="modal-dialog">
+                        <div class="modal-content">
+                        
+                        <!-- Modal Header -->
+                        <div class="modal-header">
+                        <h4 class="modal-title">Edit User</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+                        
+                        <!-- Modal body -->
+                        <div class="modal-body">
+                        <div class="container-fluid">
+                              <div class="row">
+                                    <div class="col-lg-11">
+                                    <div><?php echo SuccessMessage();
+                                                echo ErrorMessage(); ?>
+                                    </div>
+                                    <h3>Edit Orders</h3>
+                                    <?php 
+                                    $editOrderUrl=16;
+                                    // $editOrderUrl = (isset($_GET['edit']) ? $_GET['edit'] : 'error');
+                                    // echo $editOrderUrl;
+                                    $getQuery="select * from client where id='$editOrderUrl'";
+                                    $statement = $conn->prepare($getQuery);
+                                    $result =$statement->execute();
+
+                                    if($result)
+                                    {
+                                    foreach($statement as $value)
+                                                {
+                                                      $image=$value['images'];
+                                                      $attachments= $value['attachments'];
+                                          ?>
+                        
+                                    <form action="editOrder.php?id=<?php echo $editOrderUrl; ?>" id="edit-form" method="post" enctype="multipart/form-data">
+                                                      <div class="form-group">
+                                                            <input type="text" name="name" value="<?php echo $value['name'] ?>" class="form-control" placeholder="UserName">
+                                                      </div>
+                                                      <div class="form-group">
+                                                            <input type="email" name="email" value="<?php echo $value['email'] ?>" class="form-control" placeholder="Email">
+                                                      </div>
+                                                      <div class="form-group">
+                                                            <input type="number" name="phone" value="<?php echo $value['phone'] ?>" class="form-control" placeholder="Phone">
+                                                      </div>
+                                                      <div class="form-group">
+                                                            <p>Selected ProductType :<?php echo $value['product_type'] ?></p>
+                                                            <select class="form-control" name="product_type">
+                                                                  <option><?php echo $value['product_type'] ?></option>
+                                                                  <option>font color</option>
+                                                                  <option>style type</option>
+                                                            </select>
+                                                      </div>
+                                                      <div class="position-relative">
+                                                            <div class="form-group">
+                                                            <p>current-img:<img src="./<?php echo $image; ?>" style="width:40px;height:40px" alt=""/></p>
+                                                                  <div class="wrap">
+                                                                        <input type="file" name="images" accept="image/*"  />
+                                                                        <p id="filename">Images</p>
+                                                                        <span>Choose image</span>
+                                                                  </div>
+                                                            </div>
+                                                      </div>
+                                                      <div class="position-relative">
+                                                            <div class="form-group">
+                                                            <p><?php echo $attachments ?></p>
+                                                                  <div class="wrap">
+                                                                        <input type="file" id="attch" name="attachments" accept="application/*" value="<?php echo $attachments; ?>"/>
+                                                                        <p id="filenames">Attachments</p>
+                                                                        <span>Choose Attachments</span>
+                                                                  </div>
+                                                            </div>
+                                                      </div>
+                                                      <div class="form-group">
+                                                            <textarea id="my-textarea" name="message" class="form-control" 
+                                                                  placeholder="Some Thing About Yours" rows="8"><?php echo $value['message']; ?></textarea>
+                                                      </div>
+                                                      <div class="form-group">
+                                                      <input class="btn btn-primary" type="submit" value="Update Order" name="submit">
+                                                      </div>
+                                                </form>
+                                    <?php    }} ?>
+                                    </div>
+                              </div>
+                              <!-- Designer -->
+                        </div>
+                        </div>
+                        <!-- Modal footer -->
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                        </div>
+                        
+                        </div>
+                  </div>
+            </div>
+
+            <!-- Delete Model Area -->
+            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                  <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Delete Record</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                  </button>
+                  </div>
+                  <form  action="deleteClient.php" id="delete-form" method="POST" enctype="multipart/form-data">
+                        <div class="modal-body">
+                        <input type="hidden" name="delete_id" id="delete_id">
+                               Are You Sure You Want to Delete That Record...!
+                        </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                            <button type="submit" name="deleteData" class="btn btn-danger">Delete</button>
+                        </div>
+                  </form>
+            </div>
+            </div>
+            </div>
 
     <!-- Footer Area -->
     <footer class="mt-5">
@@ -92,11 +216,94 @@
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
     <script>
         $(document).ready(function (e) {
+
+            $("#edit-form").on('submit', function (e) {
+                        e.preventDefault();
+            $.ajax({
+                        type: "PUT",
+                        url: 'editOrder.php',
+                        data: new FormData(this),
+                        dataType: 'text',
+                        contentType: false,  
+                        cache: false,
+                        processData: false,
+                        success:function(response)
+                        {
+                              alert("Success"+response);
+                        },
+                        error:function(err)
+                        {
+                              alert("Error:"+err);
+                        }
+                  });
+            });
+
+    
+
+    
+      //Attachment-1
+      var fileInput = document.querySelector('input[type=file]');
+            var filenameContainer = document.querySelector('#filename');
+            var dropzone = document.querySelector('div');
+
+            fileInput.addEventListener('change', function () {
+                  filenameContainer.innerText = fileInput.value.split('\\').pop();
+            });
+            // Attachment-2 
+            var fileInputs = document.querySelector('#attch');
+            var filenameContainers = document.querySelector('#filenames');
+            var dropzones = document.querySelector('div');
+
+            fileInputs.addEventListener('change', function () {
+                  filenameContainers.innerText = fileInputs.value.split('\\').pop();
+            });
+
+//
+
+
+            //DataTable
             $('#table_data').DataTable({ });
         });
-      
-    </script>
 
+    </script>
+<script>
+        $(document).ready(function (e) {
+
+       $(".delete_btn").on("click",function(){
+           $("#deleteModal").modal('show');
+
+           $tr=$(this).closest('tr');
+
+           var data=$tr.children("td").map(function(){
+                 return $(this).text();
+           }).get();
+            console.log(data);
+             $("#delete_id").val(data[0]);
+          });
+
+            $("#delete-form").on('submit', function (e) {
+                e.preventDefault();
+                $.ajax({
+                    type: "DELETE",
+                    url: 'deleteClient.php',
+                    data: new FormData(this),
+                    dataType: 'text',
+                    contentType: false,
+                    cache: false,
+                    processData: false,
+                    success:function(response)
+                    {
+                          alert("SuccessFully Delete the Record...!"+response);
+                    },
+                    error:function(error){
+                          alert("SomeThing Going Wrong...!"+error);
+                    }
+
+                });
+            });
+        });
+
+    </script>
 </body>
 
 </html>
